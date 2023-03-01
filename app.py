c0a8f03d9631f6ea8c7fd1e89a1991c03bf881fc
Nitin Bhushan <bhushan.nitin@posteo.net>
2023-03-01 19:06:11 +0000

    updated app

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        excuse = request.form["excuse"]
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            model="text-ada-001",
+            prompt=excuse,
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +22,3 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
